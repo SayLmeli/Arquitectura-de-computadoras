802e3e50b265733b945ede29ef276800aa345f6e
SayLomeli <sagrariolomeli56@gmail.com>
2026-02-01 21:51:19 -0600

    Create compuerta1.v

---
+++ Notas de clase/Clase 29-01-2026/compuerta1.v
@@ -0,0 +1,7 @@
+//1. Creacion de modulo y definir I/O
+module compuerta1(input a, input b, output c);
+//2. Definicion de componentes internos
+    
+//3. Cuerpo del modulo, asignaciones, instancias.
+    assign c = a & b; 
+endmodule
